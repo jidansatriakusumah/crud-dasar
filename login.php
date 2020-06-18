28aefd427c6618ef99542963bcdc6ffd954f1989
unknown <jidansatriakusumah@gmail.com>
2020-06-18 22:30:49 +0700

    Ditambahkan fitur login terenskripsi dan fitur registrasi

--- login.php
+++ login.php
@@ -10,12 +10,17 @@ $connect = new mysqli("localhost", "root", "", "crud_dasar");
 if (isset($_POST["login"])) {
   $connect = new mysqli("localhost", "root", "", "crud_dasar");
 
-  $username = htmlspecialchars($_POST['username']);
-  $password = htmlspecialchars($_POST['password']);
+  $username = mysqli_real_escape_string($connect, $_POST['username']);
+  $password = mysqli_real_escape_string($connect, $_POST['password']);
 
-  if ($username == 'admin' && $password == '12345') {
-    $_SESSION["login"] = true;
-    header("Location: index.php");
+  $result = mysqli_query($connect, "SELECT * FROM admin WHERE username = '$username'");
+  $data_admin = mysqli_fetch_array($result);
+
+  if (mysqli_num_rows($result) == 1) {
+    if (password_verify($password, $data_admin['password'])) {
+      $_SESSION["login"] = true;
+      // header("Location: index.php");
+    }
   } else {
     echo "<script>alert('Username atau password salah!');</script>";
   }
@@ -30,7 +35,7 @@ if (isset($_POST["login"])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login to CRUD_Dasar</title>
+  <title>Login - CRUD_Dasar</title>
   <link rel="stylesheet" href="style/style.css">
 </head>
 
@@ -52,6 +57,9 @@ if (isset($_POST["login"])) {
       </li>
       <li>
         <button type="submit" name="login">Login</button>
+      </li>
+      <li>
+        <a href="registrasi.php">Registrasi</a>
       </li>
     </ul>
   </form>
---
+++ registrasi.php
@@ -0,0 +1,85 @@
+<?php
+
+$connect = new mysqli("localhost", "root", "", "crud_dasar");
+
+if (isset($_POST["registrasi"])) {
+  $connect = new mysqli("localhost", "root", "", "crud_dasar");
+
+  $username = mysqli_escape_string($connect, strtolower($_POST["username"]));
+  $password1 = mysqli_escape_string($connect, $_POST["password1"]);
+  $password2 = mysqli_escape_string($connect, $_POST["password2"]);
+
+  // print_r($username);
+  // print_r($password1);
+  // print_r($password2);
+
+
+  if (empty($username) || empty($password1) || empty($password2)) {
+    echo "<script>alert('Username atau password tidak boleh kosong!');</script>";
+    // header("Location: registrasi.php");
+  }
+
+  $result = mysqli_query($connect, "SELECT * FROM admin WHERE username = '$username'");
+  if (mysqli_num_rows($result) == 1) {
+    echo "<script>alert('Username sudah ada, silahkan pilih username yang lain!');</script>";
+    // header("Location: registrasi.php");
+  }
+
+  if (strlen($password1) < 5) {
+    echo "<script>alert('Password terlalu pendek!');</script>";
+    // header("Location: registrasi.php");
+  }
+
+  $password_baru = password_hash($password1, PASSWORD_DEFAULT);
+  $add = mysqli_query($connect, "INSERT INTO admin (username, password) VALUES ('$username', '$password_baru')");
+  echo "<script>alert('Selamat! Anda berhasil registrasi!');</script>";
+
+  // header("Location: login.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Registrasi - CRUD Dasar</title>
+  <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+  <h2>Registrasi</h2>
+  <form action="" method="POST">
+    <ul>
+      <li>
+        <label>
+          Username:
+          <input type="text" name="username" autocomplete="off" autofocus required>
+        </label>
+      </li>
+      <li>
+        <label>
+          Password:
+          <input type="password" name="password1" required>
+        </label>
+      </li>
+      <li>
+        <label>
+          Konfirmasi password:
+          <input type="password" name="password2" required>
+        </label>
+      </li>
+      <li>
+        <button type="submit" name="registrasi">Registrasi</button>
+      </li>
+      <li>
+        <a href="login.php">Kembali</a>
+      </li>
+    </ul>
+  </form>
+</body>
+
+</html>
